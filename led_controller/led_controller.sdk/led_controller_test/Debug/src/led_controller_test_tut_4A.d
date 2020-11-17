70640b97a7dfd7e19fa6ef17f20ce639e8c402d2
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2020-11-17 01:33:31 -0500

    Started with Tutorial 4A.

---
+++ led_controller/led_controller.sdk/led_controller_test/Debug/src/led_controller_test_tut_4A.d
@@ -0,0 +1,41 @@
+src/led_controller_test_tut_4A.o src/led_controller_test_tut_4A.o: \
+ ../src/led_controller_test_tut_4A.c \
+ ../../led_controller_test_bsp/ps7_cortexa9_0/include/led_controller.h \
+ ../../led_controller_test_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../led_controller_test_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../led_controller_test_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../led_controller_test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../led_controller_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../led_controller_test_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../led_controller_test_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../led_controller_test_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../led_controller_test_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../led_controller_test_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../led_controller_test_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../led_controller_test_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h
+
+../../led_controller_test_bsp/ps7_cortexa9_0/include/led_controller.h:
+
+../../led_controller_test_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../led_controller_test_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../led_controller_test_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../led_controller_test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../led_controller_test_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../led_controller_test_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../led_controller_test_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../led_controller_test_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../led_controller_test_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../led_controller_test_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../led_controller_test_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../led_controller_test_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
