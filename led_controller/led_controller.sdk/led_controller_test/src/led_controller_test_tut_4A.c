6ec560a9f6e71b1fcf26eb8a70897672b71b0a1c
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2020-11-17 17:22:45 -0500

    Implemented C max bit counter, tested 10 numbers.

--- led_controller/led_controller.sdk/led_controller_test/src/led_controller_test_tut_4A.c
+++ led_controller/led_controller.sdk/led_controller_test/src/led_controller_test_tut_4A.c
@@ -19,15 +19,21 @@
 #include "xil_io.h"
 
 // Number of integers to count the bits
-#define N_INTS 5
+#define N_INTS 10
 // Define delay length
-#define DELAY 100000000ul
+#define DELAY 100000000uL
 // Blank LED delay is divided by this value
 #define BLANK_INC	3
 
 
 /* 	Define the base memory address of the led_controller IP core */
 #define LED_BASE XPAR_LED_CONTROLLER_0_S00_AXI_BASEADDR
+
+/* the mask for lowest 15-bits */
+#define	LOW_MASK	0b111111111111111
+
+/* @return the length of the longest sequence of set bits in i */
+int max_seq_len(u32 i);
 
 /* main function */
 int main(void){
@@ -36,8 +42,14 @@ int main(void){
 	/* unsigned 32-bit variables for storing current LED value */
 	u32 ints[N_INTS] =
 	{ 0b011001111001011, 0b011001111001110, 0b011001111000111,
-	  0b011001111001111, 0b011011111001011
+	  0b011001111001111, 0b011011111001011, 0b000000000000000,
+	  0b011111111111111,	// 7 test values
+	  5446, 9150, 4889 		// 3 randomly generated values
 	};
+	// test values:
+	// 3 permutations of ints[0],
+	// 2 permutations of ints[0] with an extra 1
+
 	/* 32-bit for longer delay */
 	u32 i_delay=0;
 
@@ -47,8 +59,13 @@ int main(void){
 	/* Loop forever */
 	while(1){
 			while(i_int<N_INTS){
-				/* Print value to terminal */
-				xil_printf("LED value: %d\r\n", i_int);
+				/* Print index to terminal */
+				xil_printf("index  : %d\r\n", i_int);
+				/* Print integer to terminal */
+				xil_printf("integer: %d\r\n", ints[i_int]);
+				/* Print the maximum count to the terminal */
+				xil_printf("max len: %d\r\n\r\n", max_seq_len(ints[i_int]));
+
 				/* Write value to led_controller IP core using generated driver function */
 				LED_CONTROLLER_mWriteReg(LED_BASE, 0, ints[i_int]);
 				/* next integer */
@@ -68,6 +85,31 @@ int main(void){
 			for(i_delay=0;i_delay<DELAY;i_delay++);
 		}
 	return 1;
+}
+
+/* find the length of the longest sequence of set bits in i */
+int max_seq_len(u32 i) {
+	int count = 0;	/* the current count */
+	int max = 0;	/* the maximum count so far */
+	for (u32 low_bits = (i & LOW_MASK);	/* take the 15 lowest bits */
+			low_bits;					/* loop until no bits left */
+			low_bits >>= 1)				/* arithmetic shifting right by 1 */
+	{
+		/* if the LSB is 1, count*/
+		if (low_bits & 0b1) {
+			count++;
+			/* if count exceeds max, it replaces max */
+			if (max < count) {
+				max = count;
+			}
+		}
+		/* otherwise, reset the count */
+		else {
+			count = 0;
+		}
+	}
+	/* return the maximum found */
+	return max;
 }
 
 
